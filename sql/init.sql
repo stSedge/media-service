64f4df192a6d78f43c533c3d01487631c9254a33
Angelina Stiukhina <a.stiukhina@gmail.com>
2025-06-29 18:30:25 +0400

    add 'role' column to 'user' table

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(255)
 ); 
